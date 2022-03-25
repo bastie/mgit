bbac34c98225ad67f30fd001722b1d37a068ab19
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2022-03-25 16:28:01 +0100

    public struct

---
+++ Sources/mgit/bo/Repository.swift
@@ -0,0 +1,13 @@
+//
+//  Repository.swift
+//  
+//
+//  Created by Sebastian Ritter on 25.03.22.
+//
+
+import Foundation
+
+
+class Repository {
+    
+}
---
+++ Sources/mgit/mgit.init.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Administrator on 25.03.22.
+//
+
+import Foundation
