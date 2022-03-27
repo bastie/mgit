96dbc5e8e3ed386cd479518fb4cabf176bd41e8d
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2022-03-27 11:54:44 +0200

    remove build error source instead of repair it

--- Package.swift
+++ Package.swift
@@ -1,20 +1,16 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "libmgit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "libmgit",
             targets: ["libmgit"]),
         .executable(name: "mgit", targets: ["mgit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/bastie/egg.git", .upToNextMajor(from: "0.1.0")),
     ],
@@ -32,9 +28,5 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "KnightLife", package: "egg"),
             ]),
-
-        .testTarget(
-            name: "libmgitTests",
-            dependencies: ["libmgit"]),
     ]
 )
---
+++ Sources/libmgit/Objects.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Administrator on 27.03.22.
+//
+
+import Foundation
